601bc0ef37d20bf82c2761224254c37774236a16
yogeshktech <yogeshkumar0700@gmail.com>
2025-09-26 10:57:17 +0530

    update google favicond

--- resources/views/front/layouts/layout.blade.php
+++ resources/views/front/layouts/layout.blade.php
@@ -49,7 +49,7 @@
 
 <!-- Favicon for browsers and Google -->
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('front/assets/images/fevicon-icon.png') }}">
-<link rel="icon" type="image/png" sizes="48x48" href="{{ asset('front/assets/images/fevicon-icon.png') }}g">
+<link rel="icon" type="image/png" sizes="48x48" href="{{ asset('front/assets/images/fevicon-icon.png') }}">
 <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('front/assets/images/fevicon-icon.png') }}">
 
 
